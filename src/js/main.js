4600c3eae927dc8c5b4f5d408d68697ca2878b11
Oleksandr Trybrat <oleksandr.trybrat1808@gmail.com>
2025-11-29 16:50:57 +0200

    headerBlur.js with fn to class

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,10 @@
 'use strict'
 
 import { BurgerMenu } from './burgerMenu.js'
+import { HeaderBlur } from './headerBlur.js'
 
 // Ініціалізація після завантаження DOM
 document.addEventListener('DOMContentLoaded', () => {
   new BurgerMenu('#burger', 'header')
-})
-
-// ==========================================================
-// JAVASCRIPT
-// ==========================================================
-
-document.addEventListener('DOMContentLoaded', () => {
-  const header = document.querySelector('.header')
-
-  // Перевірка наявності елемента
-  if (!header) return
-
-  // Функція, яка перевіряє позицію скролу
-  function toggleHeaderShadow() {
-    // window.scrollY - це поточна вертикальна позиція прокручування
-    // Якщо позиція прокручування > 1px (тобто прокручування почалося)
-    if (window.scrollY > 1) {
-      header.classList.add('scrolled')
-    } else {
-      header.classList.remove('scrolled')
-    }
-  }
-
-  // 1. Прив'язуємо функцію до події скролу
-  window.addEventListener('scroll', toggleHeaderShadow)
-
-  // 2. Викликаємо функцію один раз при завантаженні,
-  // щоб встановити коректний стан, якщо сторінка завантажена не на початку.
-  toggleHeaderShadow()
+  new HeaderBlur('header')
 })
