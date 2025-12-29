c28d59de53442e1f0aa82a6f5d1a33a1707e9a49
Oleksandr Trybrat <oleksandr.trybrat1808@gmail.com>
2025-12-29 17:28:53 +0200

    section testimonials finish

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@
 import { BurgerMenu } from './burgerMenu.js';
 import { HeaderBlur } from './headerBlur.js';
 import { CardPagination } from './pagination.js';
+import { initTestimonialsSlider } from './slider.js';
 
 // Ініціалізація після завантаження DOM
 document.addEventListener('DOMContentLoaded', () => {
@@ -14,4 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
     '[data-control="next"]', // Селектор кнопки "Вперед"
     '.page__number',
   );
+
+  // 2. Викликаємо ініціалізацію слайдера
+  initTestimonialsSlider();
 });
